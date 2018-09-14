c49d4030ea9882b0eb6de83e7bee26beca2b574a
martinezGaetan <martinezgaetan1@hotmail.fr>
2018-09-14 09:40:34 +0200

    installation en cours

--- management/views.py
+++ management/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+ 
+class HomePageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'index.html', context=None)
 
-# Create your views here.
+
+class LinksPageView(TemplateView):
+    def get(self, request, **kwargs):
+        return render(request, 'links.html', context=None)
